483389f0bcf3d19284f9e5fef41551c2d1ee7da5
Ethan Meza <ethan7714@tamu.edu>
2026-02-25 10:56:02 -0600

    Update page.tsx

--- colorstack/app/page.tsx
+++ colorstack/app/page.tsx
@@ -1,11 +1,8 @@
 import Hero from "../components/sections/Hero";
-import Mission from "../components/sections/Mission";
-
 export default function Home() {
   return (
     <>
       <Hero />
-      <Mission />
     </>
   );
 }
